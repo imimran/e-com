6b3d034f386986ffbbf1f963588e0a9e1822a304
imimran <engr.aih@gmail.com>
2022-01-11 11:11:39 +0600

    add env

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,6 @@ export const PORT = process.env.PORT!;
 export const APP_URL = process.env.APP_URL!;
 export const MONGO_URL = process.env.MONGO_URL!;
 export const JWT_KEY = process.env.JWT_KEY!;
+export const MAIL_USER = process.env.MAIL_USER!;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD!;
 
--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,12 +1,11 @@
-
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
+import { MAIL_USER, MAIL_PASSWORD } from "../config";
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "7daaf41c2c28b1",
-      pass: "c2a857edcb6065"
-    }
-  });
-
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: MAIL_USER,
+    pass: MAIL_PASSWORD,
+  },
+});
